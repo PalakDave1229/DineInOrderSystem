1cacaf7dec8851f5a0faf9d1165f362187110584
AdityaGajjar64 <aadityamandaviya@gmail.com>
2025-02-27 16:24:49 +0530

    User,staff,Admin model adding

--- src/main/java/com/example/dine_in_order_api/model/Staff.java
+++ src/main/java/com/example/dine_in_order_api/model/Staff.java
@@ -1,14 +1,14 @@
 package com.example.dine_in_order_api.model;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name="admins")
+@Table(name="staffs")
 @Getter
 @Setter
-public class Admin{
+public class Staff {
+
 }
---
+++ src/main/java/com/example/dine_in_order_api/service/service/UserServiceImp.java
@@ -0,0 +1,20 @@
+package com.example.dine_in_order_api.service.service;
+
+import com.example.dine_in_order_api.model.User;
+import com.example.dine_in_order_api.repository.UserRepository;
+import com.example.dine_in_order_api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImp implements UserService{
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public User registration(User user) {
+        return userRepository.save(user);
+    }
+}
